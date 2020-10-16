ed806416cc043cb006a88f465616aea2063a8ab8
Karthik <manasabhat794@gmail.com>
2020-10-16 13:18:39 -0400

    Uploading Lab_5

---
+++ Lab_5/src/Business/Order.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author manasa
+ */
+public class Order {
+    private ArrayList<OrderItem> orderItemList;
+    private int orderNum ;
+    private static int count = 0;
+    
+    public Order(){
+        count++;
+        orderNum=count;
+        orderItemList = new ArrayList<OrderItem>();
+    }
+
+    public ArrayList<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(ArrayList<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(int orderNum) {
+        this.orderNum = orderNum;
+    }
+    
+    public OrderItem addOrderItem(Product p, int q, Double price){
+        OrderItem o = new OrderItem();
+        o.setProduct(p);
+        o.setQuantity(q);
+        o.setSalesPrice(price);
+        orderItemList.add(o);
+        return o;
+    }
+    
+    public void removeOrderItem(OrderItem o){
+        orderItemList.remove(o);
+    }
+}
