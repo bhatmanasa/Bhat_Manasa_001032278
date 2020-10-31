770800e2296aa062f1d1848f4462354fae0f0aa3
Karthik <manasabhat794@gmail.com>
2020-10-30 22:00:09 -0400

    Adding Lab7Skeleton

---
+++ Lab_7/src/lab7/analytics/AnalysisHelper.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7.analytics;
+
+/**
+ *
+ * @author harshalneelkamal
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lab7.entities.Comment;
+import lab7.entities.Post;
+import lab7.entities.User;
+
+
+public class AnalysisHelper {
+    //Find Average number of likes per comment.
+    //TODO
+    public void getAverageLikesPerCommets() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        int likeNumber = 0;
+        int commentNumber = comments.size();
+        for (Comment c : comments.values()) {
+            likeNumber += c.getLikes();
+        }
+        
+        System.out.println("average of likes per comments: " + likeNumber / commentNumber);
+            
+    }
+}
